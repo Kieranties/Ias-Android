09fe4d998eaca8fd3263d491e4ee274e1f55a05a
Kieranties <Kieran@Kieranties.com>
2012-03-11 17:07:09 +0000

    Application class introduced with simple preference storage model

---
+++ src/org/iasess/android/IasessApp.java
@@ -0,0 +1,40 @@
+package org.iasess.android;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.preference.PreferenceManager;
+
+public class IasessApp extends Application {
+
+	private static Context context;
+	
+	/** Application wide constants **/
+	public static final String PREFS_USERNAME = "username";
+	
+	/** Application wide properties **/
+	public static final SharedPreferences getPreferences(){
+		return PreferenceManager.getDefaultSharedPreferences(context);
+	}
+	
+	public static final String getPreferenceString(String key, String defValue){
+		return getPreferences().getString(key, defValue);
+	}
+	public static final String getPreferenceString(String key){
+		return getPreferenceString(key, "");
+	}
+	
+	public static final void setPreferenceString(String key, String value){
+		Editor editor = getPreferences().edit();
+		editor.putString(key, value);
+		editor.commit();
+	}	
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		context = getApplicationContext();		
+	}
+}

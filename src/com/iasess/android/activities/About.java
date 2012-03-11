b3be330ef85af2950311c505b44b86c9afaa0607
Kieranties <Kieran@Kieranties.com>
2012-03-11 09:15:57 +0000

    Implemented About activity and view

---
+++ src/com/iasess/android/activities/About.java
@@ -0,0 +1,22 @@
+package com.iasess.android.activities;
+
+import org.iasess.android.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+public class About extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+		
+		//Load the HTML content from resources
+		TextView tv = (TextView)findViewById(R.id.textAboutBlurb);
+		String content = getResources().getString(R.string.about_blurb);
+		tv.setText(Html.fromHtml(content));
+	}
+
+}

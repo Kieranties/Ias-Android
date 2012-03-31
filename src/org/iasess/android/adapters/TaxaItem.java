5d6f1c4e4eb06df82fda60ee5999d1ada1a22d12
Kieranties <Kieran@Kieranties.com>
2012-03-31 10:58:51 +0100

    Housekeeping - Adapter namespace

--- src/org/iasess/android/adapters/TaxaItem.java
+++ src/org/iasess/android/adapters/TaxaItem.java
@@ -4,44 +4,99 @@ import java.util.Map;
 
 import com.google.gson.annotations.SerializedName;
 
+/*
+ * Object implementation of the data returned by the API
+ * for Taxon requests.  Can be serialized/deserialized with Gson 
+ */
 public class TaxaItem {
 	
+	/*
+	 * The text details of the item
+	 */
 	@SerializedName("key_text")
 	private String keyText;
+	
+	/*
+	 * The full scientific name of the item
+	 */
 	@SerializedName("scientific_name")
 	private String scientificName;
+	
+	/*
+	 * List of sightings for the item
+	 */
 	private int[] sightings;
+	
+	/*
+	 * The rank of the item
+	 */
 	private String rank;
+	
+	/*
+	 * Map of images for the item.
+	 * Each Map entry relates to a size and set of images
+	 * of that size
+	 */
 	@SerializedName("key_images")
 	private Map<String, String[]> keyImages;
+	
+	/*
+	 * The common/simple name for the item
+	 */
 	@SerializedName("common_name")
 	private String commonName;
+	
+	/*
+	 * The unique identifier for the item
+	 */
 	private int pk;	
 	
+	
+	/*
+	 * Gets the item key text	
+	 */
 	public String getKeyTxt(){		
 		return keyText;
 	}
 	
+	/*
+	 * Gets the item scientific name	
+	 */
 	public String getScientificName(){
 		return scientificName;
 	}
 	
+	/*
+	 * Gets the item sightings	
+	 */
 	public int[] getSightings(){
 		return sightings;
 	}
 	
+	/*
+	 * Gets the item rank
+	 */
 	public String getRank(){
 		return rank;
 	}
 	
+	/*
+	 * Gets the item key images
+	 */
 	public Map<String, String[]> getKeyImages(){
 		return keyImages;
 	}
 	
+	/*
+	 * Gets the item common name
+	 */
 	public String getCommonName(){
 		return commonName;
 	}
 	
+	/*
+	 * Gets the item unique identifier	
+	 */
 	public int getPk(){
 		return pk;
 	}
--- src/org/iasess/android/adapters/TaxaItemAdapter.java
+++ src/org/iasess/android/adapters/TaxaItemAdapter.java
@@ -9,18 +9,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+/*
+ * Adapter class to populate a ListView with TaxaItems
+ */
 public class TaxaItemAdapter extends ArrayAdapter<TaxaItem> {
 
+	/*
+	 * Constructor
+	 */
 	public TaxaItemAdapter(Context context, int textViewResourceId, ArrayList<TaxaItem> items) {
 		super(context, textViewResourceId, items);
 	}
 
+	/*
+	 * Inflates each item in the view with details from TaxaItem at the given position
+	 */
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View v = convertView;
+		
+		//use the default TaxaItem list view if not defined
 		if (v == null) {
 			LayoutInflater vi = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			v = vi.inflate(R.layout.image_list_item, null);
@@ -30,13 +40,13 @@ public class TaxaItemAdapter extends ArrayAdapter<TaxaItem> {
 		if (item != null) {
 			TextView primaryTitle = (TextView) v.findViewById(R.id.textPrimary);
 			TextView secondaryTitle = (TextView) v.findViewById(R.id.textSecondary);
-			ImageView imageSpot = (ImageView) v.findViewById(R.id.icon);
+			//ImageView imageSpot = (ImageView) v.findViewById(R.id.icon);
 
 			primaryTitle.setText(item.getCommonName());
 			secondaryTitle.setText(item.getScientificName());
 			//imageSpot.setImageURI(item.getImageUri());
 			
-			//set tag to item identifier
+			//set tag to item identifier and common name for later reference
 			v.setTag(item.getPk() + "|" + item.getCommonName());
 		}
 		return v;

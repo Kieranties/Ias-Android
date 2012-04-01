ae076224115cd477464030751ab32162cb6af14c
Kieranties <Kieran@Kieranties.com>
2012-04-01 22:34:06 +0100

    Implemented AsyncTask for username checking.  Works like a charm!

--- src/org/iasess/android/activities/Settings.java
+++ src/org/iasess/android/activities/Settings.java
@@ -7,6 +7,8 @@ import org.iasess.android.api.UserCheckResponse;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -51,19 +53,56 @@ public class Settings extends Activity implements OnEditorActionListener {
 		finish();
 	}
 
+	/*
+	 * Capture events from the soft keyboard for the username edit box
+	 */
 	public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
 		if(actionId == EditorInfo.IME_ACTION_DONE){
-			// fire off the validation check
-			final ProgressDialog dialog = ProgressDialog.show(Settings.this, "", "Checking Username...", true);
-			UserCheckResponse userCheck = ApiHandler.checkUser(_editText.getText().toString());
-			dialog.cancel();
-			IasessApp.makeToast(this, userCheck.getAnswer());
-			String username = userCheck.getUsername();
+			new UsernameCheckTask().execute(_editText.getText().toString());			
+		}
+		return false;
+	}
+	
+	/*
+	 * Class to handle async requests to server
+	 */
+	private class UsernameCheckTask extends AsyncTask<String, Void, UserCheckResponse> {	
+		/*
+		 * The progress dialog to display to the user
+		 */
+		private ProgressDialog _dlg;
+		
+		/*
+		 * Executed before any processing of the task itself
+		 */
+		protected void onPreExecute() {
+			//display the dialog to the user
+			_dlg = ProgressDialog.show(Settings.this, "", "Checking Username...", true);
+	    }
+        
+		/*
+		 * The actual execution method ran in a background thread 
+		 */
+	    protected UserCheckResponse doInBackground(String... textValue) {
+	    	//return the response from the api
+	        return ApiHandler.checkUser(textValue[0]);
+	    }
+	    
+	    /*
+	     * Fired when all processing has finished
+	     */
+	    protected void onPostExecute(UserCheckResponse result) {
+	    	_dlg.dismiss();
+	    	
+	    	//inform the user of the response
+	    	IasessApp.makeToast(result.getAnswer());
+	    	
+	    	//update ui if required
+			String username = result.getUsername();
 			if(username != null && username != ""){
 				_editText.setText(username);
 			}
-			_editText.clearFocus();
-		}
-		return false;
+			_editText.clearFocus();			
+	    }
 	}
 }

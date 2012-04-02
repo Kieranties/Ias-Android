f08d14c4319ee142c0254dfd8bea7b2bf7066495
Kieranties <Kieran@Kieranties.com>
2012-04-02 20:32:17 +0100

    Updated UI to display dialog while updating taxa list

--- src/org/iasess/android/activities/SelectTaxa.java
+++ src/org/iasess/android/activities/SelectTaxa.java
@@ -9,8 +9,10 @@ import org.iasess.android.api.ApiHandler;
 import org.iasess.android.api.TaxaItem;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
@@ -27,23 +29,9 @@ public class SelectTaxa extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_taxa);
-        fillList();
-    }
-    
-    /*
-     * Requests and populates the taxa selection
-     */
-    public void fillList(){
-    	ListView listView = (ListView)findViewById(R.id.listTaxa);
-    	
-    	//request items from api
-    	ArrayList<TaxaItem> taxa = ApiHandler.getTaxa(true);
-    	
-    	//populate list
-    	TaxaItemAdapter adapter = new TaxaItemAdapter(this, R.layout.image_list_item, taxa);
-    	listView.setAdapter(adapter);    	
-    }
-    
+        new PopulateList().execute(""); // <- TODO: ugly!
+    }   
+   
     /*
      * Handles the passing of selected data to next activity
      */
@@ -58,4 +46,41 @@ public class SelectTaxa extends Activity {
     	
     	startActivity(intent);
     }
+    
+    /*
+     * Separate thread action to fetch list details
+     */
+    private class PopulateList extends AsyncTask<String, Void, ArrayList<TaxaItem>> {	
+		/*
+		 * The progress dialog to display to the user
+		 */
+		private ProgressDialog _dlg;
+		
+		/*
+		 * Executed before any processing of the task itself
+		 */
+		protected void onPreExecute() {
+			//display the dialog to the user
+			_dlg = ProgressDialog.show(SelectTaxa.this, "", "Fetching taxa...", true);
+	    }
+        
+		/*
+		 * The actual execution method ran in a background thread 
+		 */
+	    protected ArrayList<TaxaItem> doInBackground(String... params) {        	
+        	//request items from api - don't need params...
+        	return ApiHandler.getTaxa(true);
+	    }
+	    
+	    /*
+	     * Fired when all processing has finished
+	     */
+	    protected void onPostExecute(ArrayList<TaxaItem> result) {    	
+	    	//populate list
+	    	ListView listView = (ListView)findViewById(R.id.listTaxa);	    	
+        	TaxaItemAdapter adapter = new TaxaItemAdapter(SelectTaxa.this, R.layout.image_list_item, result);
+        	listView.setAdapter(adapter);   		
+        	_dlg.dismiss();
+	    }
+	}
 }
